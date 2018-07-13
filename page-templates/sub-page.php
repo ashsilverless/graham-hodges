d4ee985eb3ee2f8590f8556a01b01c81c2bf3734
Ash - SIlverless <ash@silverless.co.uk>
2018-07-13 12:32:32 +0100

    headings in sidebar sub page

--- page-templates/sub-page.php
+++ page-templates/sub-page.php
@@ -38,14 +38,14 @@ get_header(); ?>
 
 
 <div class="col-sm-3 sl-border-left sl-sidebar">
-	<p class="sidebar-heading">Related Content</p>
+	<p class="sidebar-heading">Also In This Section</p>
 	
                <?php
                 if (have_rows('page_links')):
                     while (have_rows('page_links')) : the_row();
                         ?>
                         <div class="resource">
-                            <a href="<?php the_sub_field('button_target');?>"  target="_blank"><?php the_sub_field('button_text');?></a>
+                            <a href="<?php the_sub_field('button_target');?>"  target="_blank"><?php the_sub_field('name_of_resource');?></a>
                         </div>
                         <?php
                     endwhile;
@@ -53,7 +53,7 @@ get_header(); ?>
                 endif;
                 ?>
 	
-	<p class="sidebar-heading">Also In This Section</p>
+
 	<?php get_template_part('template','parts/get-children');?>
 </div>
 
