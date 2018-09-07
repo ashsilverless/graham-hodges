4320c4f1d6d8a11cc974f5b8130c139e8759a114
Ash - SIlverless <ash@silverless.co.uk>
2018-09-07 09:41:55 +0100

    Remove scroller - blocking

--- theme/theme.js
+++ theme/theme.js
@@ -50,13 +50,7 @@ $(function() {
 
 $(document).ready(function( $ ) {
 
-$('a[href*="#"]').on('click', function (e) {
-	e.preventDefault();
 
-	$('html, body').animate({
-		scrollTop: $($(this).attr('href')).offset().top
-	}, 500, 'linear');
-});
 
 });
 
