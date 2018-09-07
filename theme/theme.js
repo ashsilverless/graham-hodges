bfaa0a20d6a891de37ee253e77f050257803f534
Ash - SIlverless <ash@silverless.co.uk>
2018-09-07 09:35:08 +0100

    Document ready for scroller

--- theme/theme.js
+++ theme/theme.js
@@ -48,12 +48,16 @@ $(function() {
   });
 });
 
+$(document).ready(function( $ ) {
+
 $('a[href*="#"]').on('click', function (e) {
 	e.preventDefault();
 
 	$('html, body').animate({
 		scrollTop: $($(this).attr('href')).offset().top
 	}, 500, 'linear');
+});
+
 });
 
 });//Don't remove
