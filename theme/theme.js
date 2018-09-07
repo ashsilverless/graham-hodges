bad17d97b0dffbc3e3f7468a9d0c4ac319f8caa0
Ash - SIlverless <ash@silverless.co.uk>
2018-09-07 10:36:12 +0100

    Add smooth scroll

--- theme/theme.js
+++ theme/theme.js
@@ -48,6 +48,15 @@ $(function() {
   });
 });
 
+$('a[href*="#"]').on('click', function (e) {
+	e.preventDefault();
+
+	$('html, body').animate({
+		scrollTop: $($(this).attr('href')).offset().top
+	}, 500, 'linear');
+});
+
+
 });//Don't remove
 
 
